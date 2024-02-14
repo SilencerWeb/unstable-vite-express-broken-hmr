b59c7475d419bb693210da0b0e87616974300d47
Max <maksim@hostaways.com>
2024-02-14 15:20:33 +0500

    adds styled components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { styled } from "styled-components";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
@@ -7,35 +8,18 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const Heading = styled.h1`
+  text-transform: uppercase;
+  color: orange;
+`;
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Where is my HRM? :(</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <Heading>Where is my HRM? :(</Heading>
+      <button onClick={() => alert("oh no, it works?")}>
+        Clicking me doesn't work
+      </button>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { unstable_vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    // Remove to fix HMR
-    react(),
-    tsconfigPaths(),
+    react({
+      include: /\.(jsx|tsx)$/,
+      babel: {
+        plugins: [['styled-components', { displayName: false }]],
+        babelrc: false,
+        configFile: false,
+      },
+    }),
     remix(),
+    tsconfigPaths(),
   ],
 });
